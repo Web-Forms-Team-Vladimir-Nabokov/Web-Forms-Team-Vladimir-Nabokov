e79e3dff0f14920422babd666b7b9d7612f25990
iliantrifonov <shogun11@abv.bg>
2014-10-21 20:43:48 +0300

    Adding Web.config to gitignore, small changes to other files

--- R2D2/R2D2.WebClient/Administration/Upload.aspx.cs
+++ R2D2/R2D2.WebClient/Administration/Upload.aspx.cs
@@ -1,4 +1,5 @@
-﻿using R2D2.Epub;
+﻿using R2D2.Data;
+using R2D2.Epub;
 using R2D2.Models;
 using System;
 using System.Collections.Generic;
@@ -12,6 +13,19 @@ namespace R2D2.WebClient.Administration
 {
     public partial class Upload : System.Web.UI.Page
     {
+        private IData data;
+
+        public Upload(IData data)
+        {
+            this.data = data;
+        }
+
+        public Upload()
+            : this(new BooksData())
+        {
+
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -70,15 +84,34 @@ namespace R2D2.WebClient.Administration
 
                 var book = new Book()
                 {
-                    
+                    Author = epubBook.Author,
+                    BookUrl = filePath,
+                    CoverUrl = epubBook.CoverUrl,
+                    DatePublished = epubBook.DatePublished,
+                    Description = epubBook.Description,
+                    Language = epubBook.Language,
+                    RatingCount = 0,
+                    RatingSum = 0,
+                    Title = epubBook.Title,
                 };
 
+                try
+                {
+                    data.Books.Add(book);
+
+                    data.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    StatusLabel.Text = "Upload status: Error saving to database!";
+                    return;
+                }
 
                 StatusLabel.Text = "Upload status: File uploaded!";
             }
         }
 
-        
+
         private string GetCurrentDateDirectoryName()
         {
             return DateTime.Now.Day.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Year.ToString();

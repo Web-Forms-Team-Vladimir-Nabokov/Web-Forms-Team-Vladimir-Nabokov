c278ce1f631d2b98e147e47a88001f2a239dffe4
iliantrifonov <shogun11@abv.bg>
2014-10-21 18:38:13 +0300

    Adding basic file upload and administration

---
+++ R2D2/R2D2.WebClient/Administration/Upload.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace R2D2.WebClient.Administration
+{
+    public partial class Upload : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void UploadButton_Click(object sender, EventArgs e)
+        {
+            if (FileUploadControl.HasFile)
+            {
+                string filename = Path.GetFileName(FileUploadControl.FileName);
+                FileUploadControl.SaveAs(Server.MapPath("~/Books/") + filename);
+                StatusLabel.Text = "Upload status: File uploaded!";
+            }
+        }
+
+    }
+}
